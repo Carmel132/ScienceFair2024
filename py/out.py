d8b54a2c4354ccd07660aac9d2f61503c0135008
Carmel <carmelbarkaee032708@gmail.com>
2025-01-02 22:26:00 -0500

    outputting maze dimensions

--- py/out.py
+++ py/out.py
@@ -1,8 +1,9 @@
 from logger import MazeAction
 
 
-def out(log: list[MazeAction], filename="out.txt"):
+def out(dim: tuple[int, int], log: list[MazeAction], filename="out.txt"):
     with open(filename, "w+") as file:
+        file.write("DIM: " + str(dim) + "\n")
         file.write("\n".join(map(repr, log)))
 
 
@@ -12,4 +13,4 @@ from state import MazeState, MazeGeneratorFactory
 s = LoggerGroup(StateLogger())
 m = MazeState(5, 5, s)
 MazeGeneratorFactory(m, 1).generate()
-out(s.log)
+out((5, 5), s.log)
