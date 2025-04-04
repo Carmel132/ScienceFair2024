38fdf9a9510e33e5d50ce413f4a9d71c88f49302
Carmel <carmelbarkaee032708@gmail.com>
2025-04-03 23:06:07 -0400

    ts bouta be lit

--- cpp/include/render.h
+++ cpp/include/render.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "runner.h"
 #include "state.h"
 #ifdef __linux__
 #include "SDL2/SDL.h"
@@ -74,34 +75,59 @@ public:
   }
 };
 
+struct GameRendererData {
+
+  std::vector<std::vector<Point>> cellPoints;
+  RenderableGroup<Rect> cellRects;
+
+  GameRendererData() : cellPoints{}, cellRects{} {}
+};
+
 struct GameRenderer {
 
   SDL_Window *window;
   SDL_Renderer *renderer;
-
+  GameRendererData data;
   void render();
 
-  std::vector<std::vector<Point>> cellPoints;
-  RenderableGroup<Rect> cellRects;
   void GenerateCellPoints(ScreenDimensions &screenDimensions);
   void GenerateCellRects(const MazeState &maze, const ScreenDimensions &dim);
   void renderCells();
 
-  std::vector<std::vector<Point>> inBetweenCellPoints;
-  RenderableGroup<Rect> inBetweenCellRects;
-  void GenerateInBetweenCellRects(const MazeState &maze,
-                                  const ScreenDimensions &dim);
-  void renderInBetweenCells();
-
-  GameRenderer() : cellPoints{}, cellRects{} {}
+  GameRenderer() : data{} {}
 
   void clear();
   void present();
 
   int initSDL();
   void shutDownSDL();
+
+private:
+  bool hasAdjacentInMaze(const Point &point, const MazeState &maze,
+                         int val) const;
 };
 
 void RenderCells(SDL_Renderer *renderer,
                  const std::vector<std::vector<Point>> &points,
                  const MazeState &maze, const ScreenDimensions &dim);
+
+// General structure to render an [ActionPattern]. Object will not render
+// unless you define a custom implementation of a given [ActionPattern] subclass
+template <class T> class RenderActionPattern : public Renderable {
+  static_assert(std::is_base_of<ActionPattern, T>(),
+                "T must inherit from ActionPattern");
+
+public:
+  void render(SDL_Renderer *renderer) override {}
+
+  RenderActionPattern(const T &_action, const MazeState &_maze,
+                      const ScreenDimensions &_dim,
+                      const GameRendererData &_data)
+      : action{_action}, maze{_maze}, dim{_dim}, data{_data} {};
+
+protected:
+  T action;
+  const MazeState &maze;
+  const ScreenDimensions &dim;
+  const GameRendererData &data;
+};
--- cpp/src/render.cpp
+++ cpp/src/render.cpp
@@ -52,10 +52,11 @@ void GameRenderer::GenerateCellPoints(ScreenDimensions &screenDimensions) {
     for (int y = 0; y < screenDimensions.cellHeight; ++y) {
       v.push_back(Point(centerOffset.x + x * dx, centerOffset.y + y * dx));
     }
-    cellPoints.push_back(v);
+    data.cellPoints.push_back(v);
   }
   screenDimensions.cellSize = dx;
-  std::cout << "Generated " << cellPoints.size() * cellPoints.at(0).size()
+  std::cout << "Generated "
+            << data.cellPoints.size() * data.cellPoints.at(0).size()
             << " points\n";
 }
 
@@ -102,24 +103,30 @@ void GameRenderer::clear() {
 
 void GameRenderer::present() { SDL_RenderPresent(renderer); }
 
+// Generates the rects of the white cells
 void GameRenderer::GenerateCellRects(const MazeState &maze,
                                      const ScreenDimensions &dim) {
-  cellRects.objs.clear();
-  for (int i = 0; i < cellPoints.size(); ++i) {
-    for (int j = 0; j < cellPoints[i].size(); ++j) {
-      if (maze.get(i, j)) {
-        Rect rect = Rect(cellPoints[i][j].x, cellPoints[i][j].y, dim.cellSize,
-                         dim.cellSize);
-        cellRects.objs.push_back(rect);
+  data.cellRects.objs.clear();
+  for (int i = 0; i < data.cellPoints.size(); ++i) {
+    for (int j = 0; j < data.cellPoints[i].size(); ++j) {
+      bool pathAroundOddEntry =
+          (i % 2 && j % 2) &&
+          !hasAdjacentInMaze(Point(i, j), maze,
+                             0); // Makes it so path moves by 3 every time
+      if (maze.get(i, j) || pathAroundOddEntry) {
+
+        Rect rect = Rect(data.cellPoints[i][j].x, data.cellPoints[i][j].y,
+                         dim.cellSize, dim.cellSize);
+        data.cellRects.objs.push_back(rect);
       }
     }
   }
 }
 
-// Renders black "empty" cells
+// Renders white wall cells
 void GameRenderer::renderCells() {
   SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xff);
-  cellRects.render(renderer);
+  data.cellRects.render(renderer);
 }
 
 void GameRenderer::render() {
@@ -128,4 +135,27 @@ void GameRenderer::render() {
   renderCells();
 
   present();
+}
+
+// Checks whether adjacent points (horizontally and vetically) in the maze
+// relative to [point] contain [val]
+bool GameRenderer::hasAdjacentInMaze(const Point &point, const MazeState &maze,
+                                     int val) const {
+  static const Point dirs[4] = {Point(1, 0), Point(0, 1), Point(-1, 0),
+                                Point(0, -1)};
+  for (Point p : dirs) {
+    int x = point.x + p.x;
+    int y = point.y + p.y;
+    if (maze.get(point.x + p.x, point.y + p.y) == val) {
+      return true;
+    }
+  }
+  return false;
+}
+
+template <> void RenderActionPattern<GetCell>::render(SDL_Renderer *renderer) {
+  Point point{data.cellPoints.at(action.loc.first).at(action.loc.second)};
+  const static Color drawColor{255, 0, 0};
+  ColoredRect drawRect{point.x, point.y, dim.cellSize, dim.cellSize, drawColor};
+  drawRect.render(renderer);
 }
