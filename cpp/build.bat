8bca0d061f9c5de4c3ab01f2157b60703494ed62
Carmel <carmelbarkaee032708@gmail.com>
2025-04-03 23:17:57 -0400

    new beginnings

--- cpp/build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set ScriptDir=%~dp0
-
-REM Launch a new console with Visual Studio environment variables set
-start "" cmd.exe /k ""C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64 && cd /d %ScriptDir% && call compile.bat build> build/compile.txt"
-
-REM Exit this script to avoid running commands in the current shell
-exit
